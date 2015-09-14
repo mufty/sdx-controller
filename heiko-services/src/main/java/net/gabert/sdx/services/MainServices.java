ecc2a0df1fcd268ed66cc4e539d47803857cd6b0
Michal Gubriansky <gubriansky@sernato.sk>
2015-09-14 14:37:26 +0200

    prep for heiko integration

--- heiko-services/src/main/java/net/gabert/sdx/services/MainServices.java
+++ heiko-services/src/main/java/net/gabert/sdx/services/MainServices.java
@@ -1,14 +1,23 @@
 package net.gabert.sdx.services;
 
-import static spark.Spark.post;
+import static spark.Spark.*;
 
+import net.gabert.sdx.heiko.core.Controller;
+import net.gabert.sdx.heiko.core.MappingService;
 import net.gabert.sdx.services.endpoints.AddDeviceService;
 import net.gabert.sdx.services.transformer.JsonTransformer;
 
 public class MainServices {
 
 	public static void main(String[] args) {
+		Controller controller = Controller.boot();
+        controller.shutDown();
+        
+        // port(5678); set different port then 4567
+        
 		post("/addDevice", (req, res) -> new AddDeviceService().call(req, res), new JsonTransformer());
+		
+		//TODO MappingService service = controller.getService(MappingService.class);
 	}
 
 }
